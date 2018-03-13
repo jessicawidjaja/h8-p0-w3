c502011afe5d2f9097a7b23149a2959c59504eea
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-13 22:05:49 +0700

    Update Exercises 14.js

--- Jumat/Exercises 14.js
+++ Jumat/Exercises 14.js
@@ -1,25 +1,21 @@
 function mengelompokkanAngka(arr) {
-var angka = [];
+  var hasil = []
   
-
-for(var i=0; i<3; i++){
-  angka.push([]); }
-  
-  for(var j= 0; j < arr.length; j++){
-    if(arr[j] % 3 === 0){ 
-    angka[2].push(arr[j]);
-      
- }  else if(arr[j] % 2 ===0){ 
-    angka[1].push(arr[j]);
-    
- }  else if(arr[j] % 2 ===0){
-   angka[0].push(arr[j]);
-
+  for(var i=0; i<3; i++){
+    hasil.push([])
   }
-  //console.log(arr[j]);
- 
-}
-  return angka ;
+  
+  for(var j=0; j<arr.length; j++){
+    if(arr[j] % 3 === 0){
+      hasil[2].push(arr[j])
+    } else if(arr[j] % 2 !== 0){
+      hasil[1].push(arr[j])
+    } else if(arr[j] % 2 === 0){
+      hasil[0].push(arr[j])
+    }
+  }
+  
+  return hasil
 }
 
 // TEST CASES
