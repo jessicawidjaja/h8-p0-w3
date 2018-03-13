0b45c16b45b3e61a37bf0a5607ae211dc73a30e4
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-13 22:06:32 +0700

    Update Exercises 14.js

--- Jumat/Exercises 14.js
+++ Jumat/Exercises 14.js
@@ -8,12 +8,12 @@ function mengelompokkanAngka(arr) {
   for(var j=0; j<arr.length; j++){
     if(arr[j] % 3 === 0){
       hasil[2].push(arr[j])
-    } else if(arr[j] % 2 !== 0){
+  } else if(arr[j] % 2 !== 0){
       hasil[1].push(arr[j])
-    } else if(arr[j] % 2 === 0){
+  } else if(arr[j] % 2 === 0){
       hasil[0].push(arr[j])
-    }
-  }
+}
+}
   
   return hasil
 }
