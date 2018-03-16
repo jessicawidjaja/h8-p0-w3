d7af8d3b30d9347c56566c3c49f180ff2a4421cf
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-16 20:47:44 +0700

    Update Exercises 14.js

--- Jumat/Exercises 14.js
+++ Jumat/Exercises 14.js
@@ -1,25 +1,25 @@
 function mengelompokkanAngka(arr) {
-  var hasil = []
+   var kelompokangka=[]
+   for(let i=0;i<3;i++){
+     kelompokangka.push([])
+   }
   
-  for(var i=0; i<3; i++){
-    hasil.push([])
-  }
-  
-  for(var j=0; j<arr.length; j++){
-    if(arr[j] % 3 === 0){
-      hasil[2].push(arr[j])
-  } else if(arr[j] % 2 !== 0){
-      hasil[1].push(arr[j])
-  } else if(arr[j] % 2 === 0){
-      hasil[0].push(arr[j])
-}
-}
-  
-  return hasil
-}
+   for(let i=0;i<arr.length;i++){
+     if(arr[i]%2===0 && arr[i]%3 !== 0){
+       kelompokangka[0].push(arr[i])
+     }
+     else if(arr[i]%2===1 && arr[i]%3 !== 0){
+       kelompokangka[1].push(arr[i])
+     }
+     else if(arr[i]%3===0){
+       kelompokangka[2].push(arr[i])
+     }
+   }
+   return kelompokangka
+ }
 
-// TEST CASES
-console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
-console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
-console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
-console.log(mengelompokkanAngka([])); // [ [], [], [] ]
+ // TEST CASES
+ console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+ console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+ console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+ console.log(mengelompokkanAngka([])); // [ [], [], [] ]
